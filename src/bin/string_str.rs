fd4a8ad244c74a5b915c6e2ec8eec1724c096f46
Oswald <oswaldsnow88@gmail.com>
2024-05-06 18:50:31 +0800

    mac

--- src/bin/string_str.rs
+++ src/bin/string_str.rs
@@ -23,12 +23,77 @@ fn main(){
     // let s1 = &s[0..2];
     // println!("{}",s1);
 
-    // push
+    // push 在尾部追加字符或字符串 
+    // 会改变原来的 string 需要可变性
     let mut s = String::from("hello ");
     s.push_str("world");
     s.push('!');
     
     println!("{}",&s);
 
+    // insert 插入单个字符或字符串
+    // 会改变原来的 string 需要可变性
+    // let mut s = String::from("中国人");
+    // /*
+    // 此处与前面一样 中 字占3个字节 1 落在 中 的字节中
+    // 所以会报错
+    //  */
+    // s.insert(1, '爱');
+    // s.insert_str(3, "爱你");
+    // println!("{}",s);
+
+    // replace 替换字符串 (要替换的字符串,新的字符串) 会替换所有匹配到的串
+    // 返回一个新的字符串 不改变原有的字符串
+    let s = String::from("karima love me , karima love me");
+    let s1 = s.replace("me", "cyy");
+    println!("{}",s1);
+
+    // replacen (要替换的字符串,新的字符串,替换个数)
+    let s2 = s.replacen("me", "cyy", 1);
+    println!("{}",s2);
+
+    // replace_range (替换范围,新的字符串) 包头不包尾
+    // 会改变原来字符串 需要可变性
+    let mut string_replace_range = String::from("I like rust!");
+    string_replace_range.replace_range(7..9, "R");
+    dbg!(string_replace_range);
+
+    // 与删除有关的方法 pop()，remove()，truncate()，clear()
+    // pop() 删除并返回字符串中最后一个字符 返回值为 Option 类型
+    let mut string_pop = String::from("rust pop 中文!");
+    if let Some(p1) = string_pop.pop(){
+        println!("{}",p1);
+    }
+
+    // remove() 删除并返回字符串中指定位置的字符 参数必须落在合适的字节位置
+    let mut string_remove = String::from("测试remove方法");
+    let remove = string_remove.remove(7);
+    dbg!(remove);
+
+    // truncate() 删除字符串从指定位置开始到结束的全部字符 参数必须落在合适的字节位置
+    // clear() 清空字符串 相当于 truncate 参数为 0 的时候
+    let mut string_truncate = String::from("测试truncate");
+    string_truncate.truncate(3);
+    dbg!(string_truncate);
+
+    // 连接
+    // 使用 + 连接，使用 + 实际上是调用了 String 的 add 方法
+    // 该方法是 String 实现了 Add trait
+    let a1 = String::from("a1 val");
+    let a2 = "a2 val";
+    let a1_a2 = a1 + a2;
+    dbg!(a1_a2);
+    // 使用 + 连接后 a1 变量所有权被转移到 add 方法中 方法调用结束后 a1 不再有效
+    // println!("{}",a1);
     
+    // 宏 format! 连接字符串
+    let s1 = "hello";
+    let s2 = String::from("rust");
+    let s = format!("{} {}!", s1, s2);
+    // format! 不会拿走变量所有权
+    println!("{}", s);
+    println!("{}",s1);
+    println!("{}",s2);
+
+
 }
