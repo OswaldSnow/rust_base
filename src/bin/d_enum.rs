42f8045c50e0a1edddb1e7c30af733a7a01fb717
Oswald <oswaldsnow88@gmail.com>
2024-05-09 23:08:36 +0800

    mac

--- src/bin/d_enum.rs
+++ src/bin/d_enum.rs
@@ -3,27 +3,63 @@ fn main(){
    枚举类型
     */
 
-    
+    // 简单定义及使用
+    let club = PokerSuit::Clubs;
+    dbg!(club);
 
+    let _u = (PokerSuit::Hearts,3);
+    
     // Option 枚举
+    // Option 被设计来替代其他语言中的 null ，所以 rust 中不存在 null 关键字
+    // 实际上下面这一行被简化了 完整的写法应该是
+    // let a = Option::Some(1);
+    let a = Some(3);
+    // None 类型也要指定 T 的类型
+    let nothing: Option<i32> = None;
+    dbg!(a);
+    // Option<T> 可以理解为：我预计它可能携带一个 T 类型的值，也可能不携带 T 这个值，这时它就是 None
+    match_something(a);
+    match_something(nothing);
+
+    if let Some(i) = a {
+        println!("解构 Option::Some(t),t is {}",i);
+    }
+
+
 
 }
 
+fn match_something(x:Option<i32>){
+    match x {
+        None => {
+            println!("this is None!!!");
+        },
+        Some(val) => {
+            println!("this bring something! It's {}",val);
+        }
+    }
+}
+
+#[derive(Debug)]
 enum PokerSuit {
     Clubs,
-    Spades,
-    Diamonds,
+    _Spades,
+    _Diamonds,
     Hearts,
 }
 
+// 可以直接将数据信息关联到枚举成员上
+#[derive(Debug)]
 enum PokerCard {
-    Clubs(u8),
-    Spades(u8),
-    Diamonds(char),
-    Hearts(char),
+    _Clubs(u8),
+    _Spades(u8),
+    _ClubsDiamonds(char),
+    _Hearts(char),
 }
 
-enum Message {
+// 枚举成员不不同类型
+// 存在普通枚举成员、结构体、元组
+enum _Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
---
+++ src/bin/e_arr_vector.rs
@@ -0,0 +1,47 @@
+fn main(){
+    /*
+    数组 和 集合
+     */
+
+    // 数组 array 数组是 rust 的基本类型
+    // 长度固定 速度快 存储在栈上
+    // 几种初始化方式
+    // 直接初始化值
+    let _arr = [1,2,3,4,5,6];
+    // 只给出存储类型和长度
+    let _arr: [u8; 3];
+    // 初始化简写方式：5个3
+    let _arr = [3; 5];
+
+    println!("1st val is {}",&_arr[0]);
+    
+    // 小技巧
+    // 使用闭包初始化这种数组，不断复制 String
+    // 编译器会根据初始化的长度 8 不断调用闭包 
+    let array: [String; 8] = std::array::from_fn(|_i| String::from("rust is good!"));
+    println!("{:#?}", array);
+
+    // 数组的切片
+    // 与 &str 切片类似 但是数组不需要注意落在有效字节内 直接按照下标进行切片
+    let a: [&str; 5] = ["陈冠希", "谢霆锋", "张柏芝", "金城武", "吴彦祖"];
+    let slice = &a[0..1];
+    dbg!(slice);
+    
+    // for in loop
+    println!("for index loop");
+    for n in 0..a.len(){
+        println!("val is {:?}",n);
+    }
+
+    println!("for val loop");
+    for v in &a{
+        println!("val is {:?}",v);
+    }
+
+    println!("for in enum loop");
+    for (i,v) in a.iter().enumerate(){
+        println!("index is {} , val is {}",i,v);
+    }
+
+
+}
